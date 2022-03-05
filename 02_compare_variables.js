ab045a1122d8f0c5a2f620a4cd27806f08e2819a
somekindofwallflower <someone.ana13@gmail.com>
2022-03-05 16:08:51 +0100

    Comparing values using == and ===

---
+++ 02_compare_variables.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// There are two ways of comparing variables in js
+
+let a = 5;
+let b = "5";
+
+console.log(a == b); // true because it checks only for value
+console.log(a === b); // false because it checks for value and type
